0b63d6fa0b0a89a4543bbc69b91947e16cb9ce46
quickavin <sharmaavin@hotmail.com>
2017-03-02 19:16:25 +0530

    Graph now shows meaningful dates and added markerView

---
+++ app/src/main/java/com/udacity/stockhawk/graph/MyMarkerView.java
@@ -0,0 +1,72 @@
+package com.udacity.stockhawk.graph;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.udacity.stockhawk.R;
+
+import java.util.Locale;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import timber.log.Timber;
+
+/**
+ * Created by Avin on 02-03-2017.
+ */
+
+public class MyMarkerView extends MarkerView {
+
+    private TextView tvContent;
+    private long referenceTimestamp;  // minimum timestamp in your data set
+    private DateFormat mDataFormat;
+    private Date mDate;
+
+    public MyMarkerView (Context context, int layoutResource, long referenceTimestamp) {
+        super(context, layoutResource);
+        // this markerview only displays a textview
+        tvContent = (TextView) findViewById(R.id.tvContent);
+        this.referenceTimestamp = referenceTimestamp;
+        this.mDataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        this.mDate = new Date();
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        long currentTimestamp = (int)e.getX() + referenceTimestamp;
+
+        tvContent.setText(e.getY() + "% at " + getTimedate(currentTimestamp)); // set the entry-value as the display text
+        Timber.d(tvContent.getText().toString());
+        //// TODO: 02-03-2017 fix markerView not showing up
+    }
+
+    private MPPointF mOffset;
+
+    @Override
+    public MPPointF getOffset() {
+
+        if(mOffset == null) {
+            // center the marker horizontally and vertically
+            mOffset = new MPPointF(-(getWidth() / 2), -getHeight());
+        }
+        return mOffset;
+    }
+
+    private String getTimedate(long timestamp){
+
+        try{
+            mDate.setTime(timestamp);
+            return mDataFormat.format(mDate);
+        }
+        catch(Exception ex){
+            return "xx";
+        }
+    }
+}
--- app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
@@ -5,14 +5,22 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.EntryXComparator;
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.graph.HourAxisValueFormatter;
+import com.udacity.stockhawk.graph.MyMarkerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    long referenceTimeStamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +40,28 @@ public class DetailsActivity extends AppCompatActivity {
         // Adding data to chart
         ArrayList<Entry> entries = new ArrayList<>();
         int i=0;
+        boolean first = true;
         for (String h: history){
             String[] split = h.split(", ");
-            entries.add(new Entry(i++, Float.parseFloat(split[1])));
+            if (first){
+                referenceTimeStamp = Long.parseLong(split[0]);
+                first = false;
+            }
+            entries.add(new Entry(Float.parseFloat(split[0])-referenceTimeStamp, Float.parseFloat(split[1])));
+
         }
+
+        Collections.sort(entries, new EntryXComparator());
         LineDataSet dataSet = new LineDataSet(entries, symbol); // add entries to dataset
         dataSet.setColor(R.color.colorAccent);
         dataSet.setValueTextColor(R.color.colorAccent); // styling
+
+        IAxisValueFormatter xAxisFormatter = new HourAxisValueFormatter(referenceTimeStamp);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(xAxisFormatter);
+
+        MyMarkerView myMarkerView = new MyMarkerView(this, R.layout.custom_marker_view, referenceTimeStamp);
+        chart.setMarker(myMarkerView);
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
